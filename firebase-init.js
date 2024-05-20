acbd603c365ceb62632f54424be81cff3c3cb373
Renan M.C <140671219+Sora018@users.noreply.github.com>
2024-05-20 19:59:01 -0300

    Update firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-P1elGjqBCwIENiCIoaYzs5i-2cKQOz8",
+  authDomain: "bd3-gui.firebaseapp.com",
+  projectId: "bd3-gui",
+  storageBucket: "bd3-gui.appspot.com",
+  messagingSenderId: "236496671949",
+  appId: "1:236496671949:web:492818b4554415a63b8584"
+};
